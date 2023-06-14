9e23de435f379b72fa34e3c2a1697644ce742798
Kaarlo Sasiang <sasiang64@gmail.com>
2023-06-15 01:40:01 +0800

    adjusted duration of toaster

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
   showConfirmButton: false,
-  timer: 1000,
+  timer: 2000,
   timerProgressBar: true,
   didOpen: (toast) => {
     toast.addEventListener('mouseenter', Swal.stopTimer)
